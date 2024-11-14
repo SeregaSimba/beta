61fd06efe709a1db949841bc4eba961237e09b0b
Sergei <143517331+SeregaSimba@users.noreply.github.com>
2024-11-14 15:40:54 +0300

    Add files via upload

---
+++ first.js
@@ -0,0 +1,69 @@
+
+
+// var nero = 'Моя первая переменная';
+// console.log(nero);
+// //document.getElementById('cull').innerHTML = ('Hello');
+// $('#cull').text(nero);
+
+
+// //$('.myclass').text(nero);
+
+// document.getElementsByClassName("myclass")[0].innerHTML = nero;
+// //document.getElementsByClassName("myclass")[0].style.color = "red";
+// $('.myclass').css('color', 'green');
+// //$('.myclass').css('background-color', 'black')
+
+// //document.getElementsByClassName('myclass')[0].style.backgroundColor = 'black';
+
+// function mySum(a, b) {
+//     return a + b;
+
+// }
+
+// console.log(mySum(7,7));
+
+// // function muSumShow(a,b){
+// //     result = a + b;
+// //     document.getElementsByClassName('myclass')[0].innerHTML = result;
+// // }
+
+// // muSumShow(5, 5);
+
+// function muSumShow(a,b){
+//     document.getElementsByClassName('myclass')[0].innerHTML = mySum(a, b);
+// }
+
+
+// muSumShow(10, 10);
+
+
+// // test.onclick = function() {
+// //     alert('привет');
+// // };
+
+// var form = function() {
+//     $('#test').fadeOut("veriSlow");  
+//     alert('Люблю Тебя');
+// }
+
+
+// //test.addEventListener('mouseover', form);
+// //test.addEventListener('contextmenu', form);
+// test.addEventListener('click', form);
+// //test.removeEventListener('click', form);
+
+// cull.addEventListener('click', function (){
+//     $('#cull').fadeOut('VeriSlow');
+//     alert('Hay ay y');
+// });
+
+// for(var i = 0; i < arrayString.length; i++) {
+//     $('#result').append(arrayString[i].toUpperCase() + '<br>');
+//   }
+
+// $('#result1').text('Вывод первого элемента массива arrayNum: ' + arrayNum[4]);
+// $('#result2').text('Вывод второго элемента массива arrayString: ' + arrayString[2]);
+// $('#result3').text('Вывод первого элемента массива arrayJson: ' + arrayJson['menu']['second']);
+
+
+
